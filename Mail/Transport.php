fee074aa9669d17db95fc92b0877bd37c3473667
Derrick Heesbeen <derrick@Derricks-MacBook-Pro.local>
2017-10-18 12:36:02 +0200

    [TASK] updated overwritten mail transporter with 2.2 feature/bugfixes

--- Mail/Transport.php
+++ Mail/Transport.php
@@ -3,67 +3,88 @@
 /**
  * A Magento 2 module named Experius/EmailCatcher
  * Copyright (C) 2016 Derrick Heesbeen
- * 
+ *
  * This file included in Experius/EmailCatcher is licensed under OSL 3.0
- * 
+ *
  * http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * Please see LICENSE.txt for the full text of the OSL 3.0 license
  */
 
-
 namespace Experius\EmailCatcher\Mail;
 
-class Transport extends \Zend_Mail_Transport_Sendmail implements \Magento\Framework\Mail\TransportInterface
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Exception\MailException;
+use Magento\Framework\Mail\MessageInterface;
+use Magento\Framework\Mail\TransportInterface;
+use Magento\Store\Model\ScopeInterface;
+
+
+class Transport implements TransportInterface
 {
 
-    protected $_message;
-    
-    protected $_emailCatcher;
-    
-    protected $_parameters;
-    
-    protected $_templateOptions;
-    
-    protected $_templateVars;
-    
-    protected $_scopeConfig;
+    const XML_PATH_SENDING_SET_RETURN_PATH = 'system/smtp/set_return_path';
+
+    const XML_PATH_SENDING_RETURN_PATH_EMAIL = 'system/smtp/return_path_email';
+
+    private $transport;
+
+    private $message;
+
+    private $scopeConfig;
+
+    private $emailCatcher;
 
     public function __construct(
-        \Magento\Framework\Mail\MessageInterface $message,
-        \Experius\EmailCatcher\Model\EmailcatcherFactory $emailCatcher,
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-        $parameters = null
-    )
-    {
+        \Zend_Mail_Transport_Sendmail $transport,
+        MessageInterface $message,
+        ScopeConfigInterface $scopeConfig,
+        \Experius\EmailCatcher\Model\EmailcatcherFactory $emailCatcher
+    ) {
         if (!$message instanceof \Zend_Mail) {
             throw new \InvalidArgumentException('The message should be an instance of \Zend_Mail');
         }
-        parent::__construct($parameters);
-        $this->_message = $message;
-        $this->_emailCatcher = $emailCatcher;
-        $this->_parameters = $parameters;
-        $this->_scopeConfig = $scopeConfig;
+
+        $this->transport = $transport;
+        $this->message = $message;
+        $this->scopeConfig = $scopeConfig;
+        $this->emailCatcher = $emailCatcher;
     }
-    
+
     public function sendMessage(\Magento\Framework\Mail\Message $message = Null)
     {
-        $message = (is_null($message)) ? $this->_message : $message;
 
-        if($this->_scopeConfig->getValue('emailcatcher/general/enabled',\Magento\Store\Model\ScopeInterface::SCOPE_STORE)) {
-            $this->_emailCatcher->create()->saveMessage($message);
+        $message = (is_null($message)) ? $this->message : $message;
+
+        if($this->scopeConfig->getValue('emailcatcher/general/enabled',\Magento\Store\Model\ScopeInterface::SCOPE_STORE)) {
+            $this->emailCatcher->create()->saveMessage($message);
         }
 
-        if(!$this->_scopeConfig->getValue('emailcatcher/general/smtp_disable',\Magento\Store\Model\ScopeInterface::SCOPE_STORE)){
+        if(!$this->scopeConfig->getValue('emailcatcher/general/smtp_disable',\Magento\Store\Model\ScopeInterface::SCOPE_STORE)){
             try {
-                parent::send($message);
+
+                $isSetReturnPath = $this->scopeConfig->getValue(
+                    self::XML_PATH_SENDING_SET_RETURN_PATH,
+                    ScopeInterface::SCOPE_STORE
+                );
+                $returnPathValue = $this->scopeConfig->getValue(
+                    self::XML_PATH_SENDING_RETURN_PATH_EMAIL,
+                    ScopeInterface::SCOPE_STORE
+                );
+
+                if ($isSetReturnPath == '1') {
+                    $this->message->setReturnPath($message->getFrom());
+                } elseif ($isSetReturnPath == '2' && $returnPathValue !== null) {
+                    $this->message->setReturnPath($returnPathValue);
+                }
+                $this->transport->send($message);
             } catch (\Exception $e) {
-                throw new \Magento\Framework\Exception\MailException(new \Magento\Framework\Phrase($e->getMessage()), $e);
+                throw new MailException(__($e->getMessage()), $e);
             }
         }
     }
 
     public function getMessage()
     {
-        return $this->_message;
+        return $this->message;
     }
 }
