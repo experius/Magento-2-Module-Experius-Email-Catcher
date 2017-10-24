2c06889aef9f4cd8d965559bdbb3cb296944600e
Derrick Heesbeen <derrick@Derricks-MacBook-Pro.local>
2017-10-24 19:02:28 +0200

    [TASK] Added intergration test

--- Test/Intergration/EmailCatcherTest.php
+++ Test/Intergration/EmailCatcherTest.php
@@ -8,34 +8,53 @@ use Magento\TestFramework\Helper\Bootstrap;
 class EmailCatcherTest extends \PHPUnit\Framework\TestCase
 {
 
+    const XML_PATH_EMAIL_SENDER = 'custom2';
+
+    const XML_PATH_EMAIL_TEMPLATE = 'test-email';
+
+    const XML_PATH_EMAIL_TO = 'emailcatcher-to@experius.nl';
+
     /**
      *
      * @magentoDataFixture Magento/Sales/_files/order.php
      * @magentoDbIsolation disabled
      * @magentoConfigFixture current_store emailcatcher/general/smtp_disable 1
+     * @magentoConfigFixture current_store system/smtp/disable 1
      *
      */
     public function testEmailCatch(){
 
-        /* @var $order \Magento\Sales\Model\Order */
-        $order = Bootstrap::getObjectManager()->create(\Magento\Sales\Model\Order::class);
-        $order->loadByIncrementId('100000001');
+        /* @var $transportBuilder \Experius\EmailCatcher\Test\Mail\Template\TransportBuilder */
+        $transportBuilder = Bootstrap::getObjectManager()->create(\Experius\EmailCatcher\Test\Mail\Template\TransportBuilder::class);
 
-        $this->assertEmpty($order->getEmailSent());
+        /* @var $scopeConfig \Magento\Framework\App\Config\ScopeConfigInterface */
+        $scopeConfig = Bootstrap::getObjectManager()->create(\Magento\Framework\App\Config\ScopeConfigInterface::class);
 
-        /* @var $emailSender \Magento\Sales\Model\Order\Email\Sender\OrderSender */
-        $emailSender = Bootstrap::getObjectManager()->create(\Magento\Sales\Model\Order\Email\Sender\OrderSender::class);
-        $emailSendResult = $emailSender->send($order);
+        $variables = [];
 
-        $this->assertTrue($emailSendResult);
+        $transport = $transportBuilder
+            ->setTemplateIdentifier(self::XML_PATH_EMAIL_TEMPLATE)
+            ->setTemplateOptions(
+                [
+                    'area' => 'frontend',
+                    'store' => 1
+                ]
+            )
+            ->setTemplateVars($variables)
+            ->setFrom(self::XML_PATH_EMAIL_SENDER)
+            ->addTo(self::XML_PATH_EMAIL_TO)
+            ->setReplyTo('derrick@experius.nl', 'Derrick')
+            ->getTransport();
 
-        $this->assertNotEmpty($order->getEmailSent());
+        $transport->sendMessage();
 
         /* @var $emailCatcher \Experius\EmailCatcher\Model\Emailcatcher */
         $emailCatcher = Bootstrap::getObjectManager()->create(\Experius\EmailCatcher\Model\Emailcatcher::class);
-        $emailCatcher->load('customer@null.com','to');
+        $emailCatcher->load(self::XML_PATH_EMAIL_TO,'to');
 
-        $this->assertEquals('customer@null.com',$emailCatcher->getTo());
+        $this->assertEquals(self::XML_PATH_EMAIL_TO,$emailCatcher->getTo());
+        $this->assertEquals('<h1>test-email</h1>',$emailCatcher->getBody());
+        $this->assertEquals('Test Email',$emailCatcher->getSubject());
 
     }
 
