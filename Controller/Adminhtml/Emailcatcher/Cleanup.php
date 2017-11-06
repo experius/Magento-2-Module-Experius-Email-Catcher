e021309211a385fc73c368318a01c243056065bb
Derrick Heesbeen <derrick@Derricks-MacBook-Pro.local>
2017-11-06 22:35:08 +0100

    [TASK] #8 added cleanup cron

---
+++ Controller/Adminhtml/Emailcatcher/Cleanup.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * A Magento 2 module named Experius/EmailCatcher
+ * Copyright (C) 2016 Derrick Heesbeen
+ *
+ * This file included in Experius/EmailCatcher is licensed under OSL 3.0
+ *
+ * http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * Please see LICENSE.txt for the full text of the OSL 3.0 license
+ */
+
+namespace Experius\EmailCatcher\Controller\Adminhtml\Emailcatcher;
+
+
+class Cleanup extends \Magento\Backend\App\Action {
+
+    protected $clean;
+
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \Experius\EmailCatcher\Cron\Clean $clean
+    ){
+        $this->clean = $clean;
+
+        parent::__construct($context);
+    }
+
+
+    public function execute(){
+
+        $resultRedirect = $this->resultRedirectFactory->create();
+
+        try {
+            $deleteCount = $this->clean->execute();
+        } catch (\Exception $e){
+            $this->messageManager->addError($e->getMessage());
+            return $resultRedirect->setPath('*/*/');
+        }
+
+        $this->messageManager->addSuccessMessage(__('Removed %1 records from %2 days ago or older',$deleteCount,'30'));
+
+        return $resultRedirect->setPath('*/*/');
+    }
+}
---
+++ Cron/Clean.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace Experius\EmailCatcher\Cron;
+
+class Clean
+{
+
+    protected $logger;
+
+    protected $resourceConnection;
+
+    /**
+     * Constructor
+     *
+     * @param \Psr\Log\LoggerInterface $logger
+     */
+    public function __construct(
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\App\ResourceConnection $resourceConnection
+    ){
+        $this->logger = $logger;
+        $this->resourceConnection = $resourceConnection;
+        $this->connection = $this->resourceConnection->getConnection();
+    }
+
+    /**
+     * Execute the cron
+     *
+     * @return void
+     */
+    public function execute()
+    {
+
+        $numberOfDays = 30;
+
+        $where = "created_at < '" . date('c', time()-($numberOfDays*(3600*24))) ."'";
+
+        $deleteCount = $this->connection->delete(
+            $this->resourceConnection->getTableName('experius_emailcatcher'),
+            $where
+        );
+
+        $this->logger->addInfo(__('Experius EmailCatcher Cleanup: Removed %1 records',$deleteCount));
+
+        return $deleteCount;
+    }
+
+
+}
