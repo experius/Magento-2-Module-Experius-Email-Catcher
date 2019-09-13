2bc70a9d589de712cd60d4b2733a321d97e0a7a0
Daphne Witmer <daphne.witmer@experius.nl>
2019-09-13 14:37:31 +0000

    [BUGFIX] Changed installschema back to old version

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -8,27 +8,21 @@
  * http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * Please see LICENSE.txt for the full text of the OSL 3.0 license
  */
-
 namespace Experius\EmailCatcher\Setup;
-
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
 use Magento\Framework\Setup\InstallSchemaInterface;
- 
+
 class InstallSchema implements InstallSchemaInterface
 {
 
-
-    
     public function install(
         SchemaSetupInterface $setup,
         ModuleContextInterface $context
     ) {
         $installer = $setup;
         $installer->startSetup();
-
         $table_experius_emailcatcher = $setup->getConnection()->newTable($setup->getTable('experius_emailcatcher'));
-
         $table_experius_emailcatcher->addColumn(
             'emailcatcher_id',
             \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
@@ -36,23 +30,20 @@ class InstallSchema implements InstallSchemaInterface
             ['identity' => true,'nullable' => false,'primary' => true,'unsigned' => true,],
             'Entity ID'
         );
-
         $table_experius_emailcatcher->addColumn(
-            'recipient',
+            'to',
             \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
             null,
             [],
             'To Email Address'
         );
-
         $table_experius_emailcatcher->addColumn(
-            'sender',
+            'from',
             \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
             null,
             [],
             'From Email Address'
         );
-
         $table_experius_emailcatcher->addColumn(
             'subject',
             \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
@@ -60,7 +51,6 @@ class InstallSchema implements InstallSchemaInterface
             [],
             'Subject'
         );
-
         $table_experius_emailcatcher->addColumn(
             'body',
             \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
@@ -68,7 +58,6 @@ class InstallSchema implements InstallSchemaInterface
             [],
             'Email Body'
         );
-
         $table_experius_emailcatcher->addColumn(
             'created_at',
             \Magento\Framework\DB\Ddl\Table::TYPE_DATETIME,
@@ -76,7 +65,6 @@ class InstallSchema implements InstallSchemaInterface
             [],
             'Created At'
         );
-
         $table_experius_emailcatcher->addColumn(
             'store_id',
             \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
@@ -84,9 +72,7 @@ class InstallSchema implements InstallSchemaInterface
             [],
             'Store Id'
         );
-
         $setup->getConnection()->createTable($table_experius_emailcatcher);
-
         $setup->endSetup();
     }
 }
