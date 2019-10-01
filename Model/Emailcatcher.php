7447ffb2a1fefc103459ed9d7f169f8832c14128
dheesbeen <derrick@experius.nl>
2019-10-01 12:10:26 +0200

    [TASK] quoted_printable_decode only for Magento 2.3.3 or higher

--- Model/Emailcatcher.php
+++ Model/Emailcatcher.php
@@ -13,18 +13,21 @@ namespace Experius\EmailCatcher\Model;
 
 class Emailcatcher extends \Magento\Framework\Model\AbstractModel
 {
-
     protected $_eventPrefix = 'experius_email_catcher';
 
     protected $_eventObject = 'email';
+
+    protected $magentoProductMetaData;
 
     public function __construct(
         \Magento\Framework\Model\Context $context,
         \Magento\Framework\Registry $registry,
         \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
         \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        \Magento\Framework\App\ProductMetadataInterface $magentoProductMetaData,
         array $data = []
     ) {
+        $this->magentoProductMetaData = $magentoProductMetaData;
 
         parent::__construct($context, $registry, $resource, $resourceCollection, $data);
     }
@@ -41,7 +44,10 @@ class Emailcatcher extends \Magento\Framework\Model\AbstractModel
         if (!method_exists($bodyObject, 'getRawContent') && method_exists($message, 'getRawMessage')) {
             $zendMessageObject = new \Zend\Mail\Message();
             $zendMessage = $zendMessageObject::fromString($message->getRawMessage());
-            $body =  quoted_printable_decode($zendMessage->getBodyText());
+            $body = $zendMessage->getBodyText();
+            if (version_compare($this->magentoProductMetaData->getVersion(), "2.3.3", ">=")) {
+                $body = quoted_printable_decode($body);
+            }
             $recipient = $this->getEmailAddressesFromObject($zendMessage->getTo());
             $sender = $this->getEmailAddressesFromObject($zendMessage->getFrom());
         } elseif (method_exists($bodyObject, 'getRawContent')) {
@@ -76,6 +82,5 @@ class Emailcatcher extends \Magento\Framework\Model\AbstractModel
 
         return $emailAddresses;
     }
+
 }
-
-
