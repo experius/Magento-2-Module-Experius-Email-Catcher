4ad177262658991ec7e7f5aa3d7d605d78cae604
Boris van Katwijk <boris@experius.nl>
2019-10-08 13:08:51 +0000

    [BUGFIX] Magento 2.2.x and 2.3.3 compatibility.

--- Model/Emailcatcher.php
+++ Model/Emailcatcher.php
@@ -17,18 +17,13 @@ class Emailcatcher extends \Magento\Framework\Model\AbstractModel
 
     protected $_eventObject = 'email';
 
-    protected $magentoProductMetaData;
-
     public function __construct(
         \Magento\Framework\Model\Context $context,
         \Magento\Framework\Registry $registry,
         \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
         \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
-        \Magento\Framework\App\ProductMetadataInterface $magentoProductMetaData,
         array $data = []
     ) {
-        $this->magentoProductMetaData = $magentoProductMetaData;
-
         parent::__construct($context, $registry, $resource, $resourceCollection, $data);
     }
 
@@ -44,10 +39,7 @@ class Emailcatcher extends \Magento\Framework\Model\AbstractModel
         if (!method_exists($bodyObject, 'getRawContent') && method_exists($message, 'getRawMessage')) {
             $zendMessageObject = new \Zend\Mail\Message();
             $zendMessage = $zendMessageObject::fromString($message->getRawMessage());
-            $body = $zendMessage->getBodyText();
-            if (version_compare($this->magentoProductMetaData->getVersion(), "2.3.3", ">=")) {
-                $body = quoted_printable_decode($body);
-            }
+            $body =  quoted_printable_decode($zendMessage->getBodyText());
             $recipient = $this->getEmailAddressesFromObject($zendMessage->getTo());
             $sender = $this->getEmailAddressesFromObject($zendMessage->getFrom());
         } elseif (method_exists($bodyObject, 'getRawContent')) {
